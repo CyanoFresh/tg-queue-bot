8abbce6f6440fd662c35b0abd1927e3adb2cba5a
Alex Solomaha <cyanofresh@gmail.com>
2019-10-30 01:06:05 +0200

    fixes

--- config/db.js
+++ config/db.js
@@ -4,20 +4,19 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: process.env.DB_LOGS === '1',
   },
   test: {
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    dialect: 'mysql'
+    dialect: "sqlite",
+    storage: ":memory:"
   },
   production: {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: process.env.DB_LOGS === '1',
   },
 };
